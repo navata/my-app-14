41d134f3e4402a2a9f0f7ca063878cb4b7b71163
Thái, Nguyễn Văn <thai.nv@pharmacity.vn>
2025-08-07 13:52:09 +0700

    Remove cookie

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
+import {
+  NextRequest,
+  //  NextResponse 
+} from 'next/server';
 
 export default async function middleware(request: NextRequest) {
   console.log('IP: ', request?.headers?.get?.('x-forwarded-for'));
   console.log('referer: ', request?.headers?.get?.('referer'));
-  const url = request.nextUrl;
-  const { pathname } = url;
+  // const url = request.nextUrl;
+  // const { pathname } = url;
 
-  request.cookies.set('COOKE_PATH_NAME', pathname);
+  // request.cookies.set('COOKE_PATH_NAME', pathname);
 
-  const response = NextResponse.next();
-  response.cookies.set(
-  	"newCookie",
-  	"004"
-  );
-  response.cookies.set('pmc-user-token', 'vi');
-  response.cookies.set('COOKE_PATH_NAME', pathname);
+  // const response = NextResponse.next();
+  // response.cookies.set(
+  // 	"newCookie",
+  // 	"004"
+  // );
+  // response.cookies.set('pmc-user-token', 'vi');
+  // response.cookies.set('COOKE_PATH_NAME', pathname);
 
-  return response
+  // return response
 }
 
 
