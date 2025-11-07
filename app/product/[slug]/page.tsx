eb1c437ca38be6e9fde67b1fa725a4a8e47166e6
Thái, Nguyễn Văn <thai.nv@pharmacity.vn>
2025-11-07 13:14:52 +0700

    Async time

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,12 +1,15 @@
+const getTime = async () => new Date().getTime();
+
 export default async function ProductDetail({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
+  const time = await getTime();
   return (
     <div>
-      My Post: {slug} + {new Date().getTime()}
+      My Post: {slug} + {time}
     </div>
   );
 }
