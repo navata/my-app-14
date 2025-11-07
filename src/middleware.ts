0ed659f5eef496349f9a62332980a7ddaf1c6fc7
Thái, Nguyễn Văn <thai.nv@pharmacity.vn>
2025-11-07 14:49:11 +0700

    Test next-intl

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,38 @@
+import {NextRequest} from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
 
-export default createMiddleware(routing);
+const handleI18nRouting = createMiddleware(routing);
+
+export default async function middleware(request: NextRequest) {
+  const headers = request.headers;
+  const logData = {
+    ip: headers.get('x-forwarded-for'),
+    host: headers.get('x-forwarded-host'),
+    url: request.url,
+    referer: headers.get('referer'),
+    userAgent: headers.get('user-agent')
+  };
+
+  console.warn(
+    JSON.stringify(
+      {
+        timestamp: new Date().toLocaleString('vi-VN', {
+          timeZone: 'Asia/Ho_Chi_Minh'
+        }),
+        level: 'INFO',
+        context: 'Request information',
+        data: logData
+      },
+      null
+    )
+  );
+
+  const response = handleI18nRouting(request);
+  response.headers.set('Cache-Control', 'public, max-age=20');
+
+  return response;
+}
 
 export const config = {
   matcher: [
