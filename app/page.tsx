54c6d6dc0d2627ab8b7fa0a40858fb2aff638004
Thái, Nguyễn Văn <thai.nv@pharmacity.vn>
2025-11-07 13:23:20 +0700

    Prefetch false

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,15 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-2 p-4">
       <Link href="/intro">Intro</Link>
-      <Link href="/product/a">Product a</Link>
-      <Link href="/product/b">Product a=b</Link>
-      <Link href="/product/c">Product a=b</Link>
+      <Link href="/product/a" prefetch={false}>
+        Product a
+      </Link>
+      <Link href="/product/b" prefetch={false}>
+        Product a=b
+      </Link>
+      <Link href="/product/c" prefetch={false}>
+        Product a=b
+      </Link>
     </div>
   );
 }
